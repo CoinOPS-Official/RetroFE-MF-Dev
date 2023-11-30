c2e86a59ac1e8ddea1825733c7903b9863da0bdf
monkofthefunk <tobias.ca@gmail.com>
2023-11-29 21:11:56 -0800

    fixed settings collection asset refresh

--- RetroFE/Source/RetroFE.cpp
+++ RetroFE/Source/RetroFE.cpp
@@ -942,6 +942,8 @@ bool RetroFE::run( )
                     }
                 }
                 currentPage_->selectPlaylist(settingsPlaylist);
+                currentPage_->onNewItemSelected();
+                currentPage_->reallocateMenuSpritePoints(); // update menu
             }
             break;
         // Next page; start onMenuExit animation
